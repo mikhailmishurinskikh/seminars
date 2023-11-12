5e0e9d002704b486af5d70b3f234a8c85374166e
Mikhail <mishurinskikh.mn@phystech.edu>
2023-11-12 18:10:54 +0300

    corrected Vector

--- sem7/1-1.py
+++ sem7/1-1.py
@@ -1,24 +1,4 @@
-class Vector():
-    def __init__(self, x, y, z, m):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.m = m
-    def __abs__(self):
-        return (self.x ** 2 + self.y ** 2 + self.z ** 2)**0.5
-    def __add__(self, other):
-        if isinstance(other, Vector):
-            return Vector(self.x + other.x, self.y + other.y, self.z + other.z, self.m)
-        elif isistance(other, int) or isistanse(other, float):
-            return Vector(self.x + other, self.y + other, self.z + other, self.m)
-    def __radd__(self, other):
-        return self + other
-    def __str__(self):
-        return f'x = {self.x} y = {self.y} z = {self.z}'
-    def __mul__(self, other):
-        return Vector(self.x * other, self.y * other, self.z * other, self.m)
-    def __truediv__(self, other):
-        return Vector(self.x / other, self.y / other, self.z / other, self.m)
+from Vector import Vector
 
 N = int(input('Количество точек:  '))
 data = []
--- sem7/1-2.py
+++ sem7/1-2.py
@@ -1,32 +1,10 @@
-#Среди данных точек найдите три точки, образующие треугольник с наибольшей площадью. Выведите данную площадь.
-class Vector():
-        def __init__(self, x, y, z):
-            self.x = x
-            self.y = y
-            self.z = z
-        def __abs__(self):
-            return (self.x**2 + self.y**2 + self.z**2)**0.5
-        def __add__(self, other):
-            if isinstance(other, Vector):
-                return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
-            elif isinstance(other, int) or isinstance(other, float):
-                return Vector(self.x+other,self.y+other,self.z+other)
-        def __radd__(self, other):
-            return self + other
-        def __sub__(self, other):
-            return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
-        def __str__(self):
-            return f'x = {self.x} y = {self.y} z = {self.z}'
-        def __matmul__(self, other): #a @ b
-            return Vector(
-                    self.y * other.z - other.y * self.z, 
-                    self.z * other.x - other.z * self.x,
-                    self.x * other.y - other.x * self.y)
+from Vector import Vector
+
 n = int(input('Number of dots: '))
 data = []
 for i1 in range(n):
     x, y, z = map(int, input().split())
-    data.append(Vector(x, y, z))
+    data.append(Vector(x, y, z, 1))
 max_sq = 0
 for i in range(len(data)):
     for j in range(i+1, len(data)):
